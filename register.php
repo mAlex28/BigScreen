c744c6f09efb3f18fc77528e4b4a9f0d81dac63f
Alex <52848335+mrash28@users.noreply.github.com>
2021-03-12 20:29:49 +0530

    modified

--- register.php
+++ register.php
@@ -1,6 +1,7 @@
 <?php
-
+    require_once('config.php')
 ?>
+
 
 <html lang="en">
 <head>
@@ -14,6 +15,29 @@
     <script src="https://kit.fontawesome.com/338bf2ef1a.js" crossorigin="anonymous"></script>
 </head>
 <body data-spy="scroll" data-target="#navbarResponsive">
+    <div>
+        <?php
+            if (isset($_POST['register'])) {
+                $fname = $_POST['fname'];
+                $lname = $_POST['lname'];
+                $email = $_POST['email'];
+                $user = $_POST['user'];
+                $pass = $_POST['password'];
+                $confpass = $_POST['confpass'];
+
+
+                $sql = "INSERT INTO User(fname, lname, email, username, password) VALUES (?,?,?,?,?)";
+                $stmtinsert = $db->prepare($sql);
+                $result = $stmtinsert->execute([$fname, $lname, $email, $user, $pass]);
+                if ($result) {
+                    echo 'Successfully saved';
+                } else {
+                    echo 'There were errors';
+                }
+            }
+        ?>
+    </div>
+
     <div class="container-fluid p-0">
         <!-- Navigation bar -->
         <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
@@ -48,52 +72,68 @@
 
     <!-- Register -->
     <div class="register">
-        <form action="" method="POST" class="box">
+        <form action="register.php" method="POST" class="box">
             <h1>Register</h1>
-            <input type="text" placeholder="First Name" name="name">
-            <input type="text" placeholder="Last Name" name="name">
-            <input type="text" placeholder="Email" name="name">
-            <input type="text" placeholder="Username" name="name">
-            <input type="password" placeholder="Password" name="password">
-            <input type="password" placeholder="Confirm password" name="password">
-            <input class="btn" type="submit" value="Register">
+            <input type="text" id="fname" placeholder="First Name" name="fname" required>
+            <input type="text" id="lname" placeholder="Last Name" name="lname" required>
+            <input type="email" id="email" placeholder="Email" name="email" required>
+            <input type="text" id="user" placeholder="Username" name="user" required>
+            <input type="password" id="password" placeholder="Password" name="password" required>
+            <input type="password" id="confpass" placeholder="Confirm password" name="confpass" required>
+            <input class="btn" type="submit" value="Register" name="register" id="register">
             Have an account? <a href="login.php">Sign In</a> <br>
         </form>
     </div>
-    
-        
-
-    <!-- Footer -->
-    <footer class="text-white" style="background-color:#141414e0;">
-        <div class="container p-4">
-            <div class="row">
-                <div class="col-lg-7 col-md-12 mb-4 mb-md-0">
-                     <!-- About -->
-                    <h5 class="text-uppercase">About</h5>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum repellat <br> 
-                        quaerat voluptatibus placeat nam, <br>
-                        commodi optio pariatur est quia magnam eum harum corrupti <br>
-                        dicta,  aliquam sequi voluptate quas.
-                    </p>
-                </div>
-                <div class="social col-lg-5 col-md-12 mb-4 mb-md-0">
-                    <!-- Social Media -->
-                    <h5 class="text-uppercase">Follow Us</h5>
-                    <a href="https://www.facebook.com/" class="fab fa-facebook-square fa-2x pr-2 text-decoration-none" target="blank"></a>
-                    <a href="https://www.twitter.com/" class="fab fa-twitter fa-2x pr-2 text-decoration-none" target="blank"></a>
-                    <a href="https://www.instagram.com/" class="fab fa-instagram fa-2x pr-2 text-decoration-none" target="blank"></a>
-                </div>
-            </div>
-        </div>
-        <div class="text-center p-3" style="background-color: rgb(253, 6, 6)">
-        © 2020 Copyright:<a class="text-white" href="#">BigScreen.com</a>
-        </div>
-    </footer>
 
     <!-- Script files -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <script type="text/javascript">
+        $(function() {
+            $('#register').click(function(e){
+                var valid = this.form.checkValidity();
+
+                if(valid) {
+                    var fname = $('#fname').val();
+                    var lname = $('#lname').val();
+                    var email = $('#email').val();
+                    var user = $('#user').val();
+                    var pass = $('#pass').val();
+                    var confpass = $('#confpass').val();
+                    e.preventDefault();
+
+                    $.ajax({
+                        type: 'POST',
+                        url: 'process.php',
+                        data: {fname: fname, lname: lname, email: email, user: user, pass: pass},
+                        success: function(data) {
+                            Swal.fire({
+                                'title': 'Successfull',
+                                'text' : data,
+                                'icon' : 'success'
+                            })
+                        },
+                        error: function(data) {
+                            wal.fire({
+                                'title': 'Error',
+                                'text' : 'User Registration failed',
+                                'icon' : 'error'
+                            })
+                        }
+                    });
+
+                    alert('true');
+                } else {
+                    alert('false');
+                }
+
+
+            });
+           
+        })
+    </script>
+
 </body>
 </html>
