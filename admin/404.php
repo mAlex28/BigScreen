ec0172cc08db325d4c2c6a45ed1b6b27ef70c157
Alex <52848335+mrash28@users.noreply.github.com>
2021-05-04 21:25:33 +0530

    error page

---
+++ admin/404.php
@@ -0,0 +1,22 @@
+<?php
+// include('security.php');
+include('includes/header.php');
+
+?>
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+
+<!-- 404 Error Text -->
+<div class="text-center">
+    <div class="error mx-auto" data-text="404">404</div>
+    <p class="lead text-gray-800 mb-5">Page Not Found</p>
+    <p class="text-gray-500 mb-0">The page you are looking for is not available</p>
+    <a href="../index.html">&larr; Back to Homepage</a>
+</div>
+
+</div>
+<!-- /.container-fluid -->
+
+<?php
+// include('includes/footer.php')
+?>
