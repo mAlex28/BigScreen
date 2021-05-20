a5579303b657e2ba3a2539f7c00a984bccf47d0b
Alex <52848335+mrash28@users.noreply.github.com>
2021-05-21 02:04:37 +0530

    modified

--- js/app.js
+++ js/app.js
@@ -1,41 +1,45 @@
-(function ($, document, window) {
+(function($, document, window){
+	
+	$(document).ready(function(){
 
-    $(document).ready(function () {
+		// Cloning main navigation for mobile menu
+		$(".mobile-navigation").append($(".main-navigation .menu").clone());
 
-        // cloing main navigation for mobile menu
-        $(".mobile-navigation").append($(".main-navigation .menu").clone());
+		// Mobile menu toggle 
+		$(".menu-toggle").click(function(){
+			$(".mobile-navigation").slideToggle();
+		});
+		$(".search-form button").click(function(){
+			$(this).toggleClass("active");
+			var $parent = $(this).parent(".search-form");
 
-        // mobile menu toggle
-        $(".menu-toggle").click(function () {
-            $(".moblile-navigation").slideToggle();
-        });
-        $(".search-form button").click(function () { 
-            $(this).toggleClass("active");
-            var $parent = $(this).parent(".search-form");
+			$parent.find("input").toggleClass("active").focus();
+		});
 
-            $parent.find("input").toggleClass("active").focus();           
-        });
 
-        $(".slider").flexslider({
-            controlNav: false,
-            prevText: '<i class="fa fa-chevron-left"></i>',
-            nextText: '<i class="fa fa-chevron-right"></i>',
-        });
-        if ($(".map").length) {
-            $('.map').gmap3({
-                map: {
-                    options: {
-                        maxZoom: 14
-                    }
-                },
-                marker: {
-                    address: "40 Baker Street, London",
-                }
-            }, "autofit" );
-        }
-      });
+		$(".slider").flexslider({
+			controlNav: false,
+			prevText:'<i class="fa fa-chevron-left"></i>',
+			nextText:'<i class="fa fa-chevron-right"></i>',
+		});
+		if( $(".map").length ) {
+			$('.map').gmap3({
+				map: {
+					options: {
+						maxZoom: 14 
+					}  
+				},
+				marker:{
+					address: "40 Sibley St, Detroit",
+				}
+			},
+			"autofit" );
+	    	
+	    }
+	});
 
-      $(window).load(function() {
+	$(window).load(function(){
 
-      });
-  }) (jQuery, document, window);
+	});
+
+})(jQuery, document, window);
