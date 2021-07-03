cdd0f7122edfddd067503410fd44fae7a7c41191
mrash28 <alexthegeek2001@gmail.com>
2021-07-04 00:45:41 +0530

    modified

--- dbconfig.php
+++ dbconfig.php
@@ -1,16 +1,26 @@
 <?php
-$server_name = "localhost";
-$db_username = "root";
-$db_password = "";
-$db_name = "Big_Screen";
+// $server_name = "localhost";
+// $db_username = "root";
+// $db_password = "";
+// $db_name = "Big_Screen";
 
-$con = mysqli_connect($server_name,$db_username,$db_password);
-$dbconfig = mysqli_select_db($con,$db_name);
+// $con = mysqli_connect($server_name,$db_username,$db_password);
+// $dbconfig = mysqli_select_db($con,$db_name);
 
-if($dbconfig) {
-    // echo "Database connected";
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("mysql://b32ac7d0290999:73ab2899@eu-cdbr-west-01.cleardb.com/heroku_8734def0dbf3177?reconnect=true"));
+$cleardb_server = $cleardb_url["eu-cdbr-west-01.cleardb.com"];
+$cleardb_username = $cleardb_url["b32ac7d0290999"];
+$cleardb_password = $cleardb_url["73ab2899"];
+$cleardb_db = substr($cleardb_url["heroku_8734def0dbf3177"], 1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password);
+$dbconfig = mysqli_select_db($con, $cleardb_db);
+
+if ($dbconfig) {
+    echo "Database connected";
 } else {
-    echo 'connection failed';
+    echo "connection failed";
 }
-
-?>
